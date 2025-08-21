293b200db7de09d4b5a4120aaa8ab44c0553104d
Dmytro Wasilewskii <diwasilewskii@gmail.com>
2025-08-21 18:51:17 +0300

    up

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Анімація появи при скролі
+window.addEventListener("scroll", function() {
+  const about = document.querySelector(".about-content");
+  const position = about.getBoundingClientRect().top;
+  const screenPos = window.innerHeight / 1.2;
+
+  if (position < screenPos) {
+    about.classList.add("visible");
+  }
+});
+
+// Ініціалізація AOS (Animate On Scroll)
+AOS.init({
+  duration: 800,
+  once: true
+});
